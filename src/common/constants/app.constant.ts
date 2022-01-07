9d4f1c87b0cc9d65029153f2a23024eedda12fc9
Daniel Mwakanema <danielmwakanema95@gmail.com>
2022-01-07 10:40:44 +0200

    chore(common): add constants

---
+++ src/common/constants/app.constant.ts
@@ -0,0 +1,4 @@
+export const MEDIATOR_NAME = 'ADX Mediator v2';
+export const MEDIATOR_API_DESCRIPTION =
+  'NestJS port of Aggregated Data Exchange mediator.';
+export const MEDIATOR_API_TAG = 'mediator-api';
