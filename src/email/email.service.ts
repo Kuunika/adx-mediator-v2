95b86c29fe4407c42ed21bfa7b76cce93a25301d
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2023-06-02 08:06:39 +0200

    ft: new email template

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createTransport } from 'nodemailer';
 import { LoggingService } from '../logging/logging.service';
+import { Conflict } from '../dhis2/types/migration-summary';
+import { MigrationSummary } from '../migration/types/migration-summary';
+import { AdxMigrationEventPayload } from '../adx/adx.controller';
+import { AdxMigrationPayloadDto } from 'src/common/dtos';
 
 export type MailAttachment = {
   filename: string;
@@ -23,33 +27,98 @@ export class EmailService {
   ) {}
 
   async send(
-    migrationSummary: string,
-    reportingPeriod: string,
-    //attachments?: MailAttachment[],
+    migrationSummary: MigrationSummary,
+    { client, migrationPayload }: AdxMigrationEventPayload,
   ) {
     try {
       const info = await this.transporter.sendMail({
         from: this.config.get<string>('EMAIL_USER'),
         //attachments,
-        to: this.config.get<string>('MAIL_LIST'),
-        subject: migrationSummary,
-        text: `Migration Successfully Completed For Reporting Period: ${reportingPeriod}
-              
-              Please find attached the migration reports for the reporting period.
-
-              Regards,
-              DHD Team
-              
-              This message was sent by an by an automated system. Please do not reply to this email.
-              for any questions or concerns please contact Brett Onions at bonions@kuunika.org
-              `,
+        //TODO: the to section need to be set programmatically
+        to: this.config.getOrThrow<string>('MAIL_LIST'),
+        bcc: this.config.getOrThrow<string>('BCC_EMAIL'),
+        subject: `Migration Completed for ${client} - ${migrationPayload?.description}`,
+        text: this.createEmailMessage(migrationPayload, migrationSummary),
       });
-      //TODO: The message id is not showing up in the logs
       this.log.info('Message sent: %s', info.messageId);
     } catch (err) {
       const error = err as Error;
       this.log.error('There was an issue sending the email');
       this.log.error(error.message);
     }
+  }
+
+  createEmailMessage(
+    migrationPayload: AdxMigrationPayloadDto,
+    migrationSummary: MigrationSummary,
+  ) {
+    return `
+    Data Migration for ${migrationPayload?.description} for Period: ${
+      migrationPayload['reporting-period']
+    }
+    Total Imported Records: ${
+      migrationSummary.importCount.imported +
+      migrationSummary.importCount.updated
+    }
+
+    ${this.createConflictsMessage(
+      migrationSummary.importCount.ignored,
+      migrationSummary.conflicts,
+    )}
+
+    Regards,
+    Malawi Ministry Of Health - Digital Health Division
+    `;
+  }
+
+  createConflictsMessage(numberIgnored: number, conflicts: Conflict[]): string {
+    const orgUnits = new Set<string>();
+    const importConflicts = new Set<string>();
+
+    conflicts.forEach((conflict) => {
+      if (conflict.value === 'Org unit not found or not accessible') {
+        orgUnits.add(conflict.object);
+      } else {
+        importConflicts.add(JSON.stringify(conflict));
+      }
+    });
+    const lineBreaks = '_______________________________';
+    return (
+      `
+    ${lineBreaks}
+    Conflicts During Migration
+    ${lineBreaks}
+    Records Ignored: ${numberIgnored}
+    ${lineBreaks}
+    ${
+      orgUnits.size !== 0
+        ? `Conflicting OrgUnits\n${lineBreaks}\n
+      ${Array.from(orgUnits)
+        .map((org) => `- ${org}`)
+        .join('\n')}\n${lineBreaks}`
+        : ''
+    }
+
+    ${
+      importConflicts.size !== 0
+        ? `Other Conflicts:
+      ${lineBreaks}
+        ${Array.from(importConflicts).reduce((acc, cur) => {
+          try {
+            const conflict: Conflict = JSON.parse(cur) as Conflict;
+            return `${acc}\n - ${conflict.object} ${conflict.value}`;
+          } catch (error) {
+            this.log.error(
+              'There was an issue during the JSON Parse for the conflicts',
+            );
+            return acc;
+          }
+        }, '')}`
+        : ''
+    }
+    `
+        //this replaces is meant to remove the white space at the beginning of each line in the message
+        .replace(/^\s+/gm, '')
+    );
   }
 }
